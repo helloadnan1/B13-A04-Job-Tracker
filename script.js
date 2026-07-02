32f41c3944de8186ee6dc200c0caf3dfe6616f21
Adnan Shahariar <adnansahrear@gmail.com>
2026-07-03 01:21:26 +0600

    header content and initial card made

---
+++ script.js
@@ -0,0 +1,20 @@
+
+let totalCount = document.getElementById("totalCount");
+let interviewCount = document.getElementById("InterviewCount");
+let rejectedCount = document.getElementById("rejectedCount");
+
+const allCards = document.getElementById("all-cards");
+
+let interviewCount = document.getElementById("interview").addEventListener("click", );
+
+
+
+function calculateCount(){
+    totalCount.innerText = allCards.children.length;
+    interviewCount.innerText = 
+
+}
+
+calculateCount()
+
+
